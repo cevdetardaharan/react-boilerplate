f4b9e30216471c827501fe34c75a86a89623791a
Cevdet Arda Haran <cevdet@tutamail.com>
2023-04-08 11:31:23 +0300

    Initializing project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>React Boilerplate</h1>
+      <p>React + Vite + SWC + TypeScript + PNPM + Rome</p>
+    </div>
+  );
+}
+
+export default App;
